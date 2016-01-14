0ad2c44153f08ed3d6c8fbbe30020c43a8f7a3a9
MJDevelopZ <mjdevelopz@gmail.com>
2016-01-14 15:37:57 +0300

    Deteling to try to figure out what went wrong

--- Ruby 104 CHallenges/first_script.rb
+++
@@ -1,3 +0,0 @@
-puts "Heck yeah this is my first ruby script."
-
-puts "Hello, World!"
--- Ruby 104 CHallenges/love.rb
+++
@@ -1,8 +0,0 @@
-Inf = 1/0.0
-Inf.class
-Float::INFINITY
-time = Time.new
-
-while (time < Inf)
-	puts "I love you"
-end
